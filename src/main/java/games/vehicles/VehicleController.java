8b297aaa9dc7888f82bc41bdeb37d8ddcdf5e698
Paulina <milewska.p@wp.pl>
2020-02-25 15:39:55 +0100

    Add CRUD methods in VehicleController and WeaponController

--- src/main/java/games/vehicles/VehicleController.java
+++ src/main/java/games/vehicles/VehicleController.java
@@ -1,7 +1,101 @@
 package games.vehicles;
 
+import games.heroes.Hero;
+import games.heroes.HeroDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class VehicleController {
+
+    private List<Vehicle> list = new ArrayList<>();
+    private List<Hero> listHero;
+
+    @Autowired
+    private HeroDao heroDao;
+    @Autowired
+    private VehicleDao vehicleDao;
+
+    public VehicleController(VehicleDao vehicleDao, HeroDao heroDao ) {
+        this.vehicleDao = vehicleDao;
+        this.heroDao = heroDao;
+        listHero = heroDao.getHeroes();
+    }
+
+    @RequestMapping(value = "/vehicleform", method = RequestMethod.GET)
+    public String showform(Model model) {
+        listHero = heroDao.getHeroes();
+        model.addAttribute("vehicle", new Vehicle());
+        model.addAttribute("listHero", listHero);
+        return "vehicle/vehicleform";
+    }
+
+    @RequestMapping("/savevehicle")
+    public ModelAndView saveCar(@ModelAttribute(value = "vehicle") Vehicle vehicle,
+                                @ModelAttribute(value = "vehicle.id") String vehicle_id_Sting) {
+        int vehicle_id = Integer.parseInt(vehicle_id_Sting);
+        listHero = heroDao.getHeroes();
+
+        vehicle.setHeroes(listHero.get(vehicle_id));
+        if (vehicle.getId() == 0) {
+
+            vehicle.setId(list.size()+1);
+            vehicleDao.createVehicle(vehicle);
+            vehicle.setId(list.size());
+            list.add(vehicle);
+
+        } else {
+
+            listHero = heroDao.getHeroes();
+            int index = vehicle.getId();
+            vehicle.setHeroes(listHero.get(index));
+            vehicleDao.updateVehicle(vehicle);
+
+//            }
+            list.set(vehicle.getId() - 1, vehicle);
+            updateVehicleInList(vehicle);
+        }
+        return new ModelAndView("redirect:/viewvehicle");
+    }
+
+    Hero hero;
+
+    @RequestMapping(value = "/deletevehicle", method = RequestMethod.POST)
+    public ModelAndView delete(@RequestParam(value = "vehicle_id") String vehicle_id) {
+        Integer vehicleId = Integer.parseInt(vehicle_id);
+        Vehicle vehicleToDelete = getVehicleById(vehicleId);
+
+        vehicleDao.deleteVehicle(vehicleToDelete);
+        list.remove(vehicleToDelete);
+        return new ModelAndView("redirect:/viewvehicle");
+    }
+
+    @RequestMapping("/viewvehicle")
+    public ModelAndView viewvehicle(Model model) {
+        list = vehicleDao.getVehicles();
+        return new ModelAndView("vehicle/viewvehicle", "list", list);
+    }
+
+    private Vehicle getVehicleById(@RequestParam Integer vehicle_id) {
+        return list.stream().filter(f -> f.getId() == vehicle_id).findFirst().get();
+    }
+
+    private void updateVehicleInList(Vehicle vehicle) {
+        Vehicle vehicleTemp = getVehicleById(vehicle.getId());
+        vehicleTemp.setName(vehicle.getName());
+        vehicleTemp.setSpeed(vehicle.getSpeed());
+        vehicleTemp.setValue(vehicle.getValue());
+        vehicleTemp.setHeroes(vehicle.getHeroes());
+
+    }
+
 }
--- src/main/java/games/weapons/WeaponController.java
+++ src/main/java/games/weapons/WeaponController.java
@@ -1,7 +1,86 @@
 package games.weapons;
 
+import games.heroes.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.Integer.parseInt;
 
 @Controller
 public class WeaponController {
+    private List<Weapon> weaponList;
+    private List<Hero> heroList;
+    @Autowired
+    WeaponDao weaponDao;
+
+    public WeaponController() {
+        weaponList = new ArrayList<>();
+        heroList = new ArrayList<>();
+    }
+
+    @RequestMapping(value = "/weaponform", method = RequestMethod.GET)
+    public String showform(Model model) {
+        model.addAttribute("weapon", new Weapon());
+        return "weapon/weaponform";
+    }
+
+    @RequestMapping("/saveweapon")
+    public ModelAndView save(@ModelAttribute(value = "weapon") Weapon weapon,
+                             @ModelAttribute(value = "heroIds") ArrayList<Integer> heroIds) {
+        Set<Hero> heroHashSet = new HashSet<>();
+        for (Hero hero : heroList) {
+            if (weapon.getId() == 0) {
+                if (heroIds.contains(hero.getId())) {
+                    heroHashSet.add(hero);
+                }
+            }
+            weapon.setHeroSet(heroHashSet);
+
+            if (weapon.getId() == 0) {
+                weaponDao.createWeapon(weapon);
+
+                weapon.setId(weaponList.size());
+                weaponList.add(weapon);
+            } else {
+
+                weaponDao.updateWeapon(weapon);
+                weaponList.set(weapon.getId() - 1, weapon);
+            }
+        }
+        return new ModelAndView("redirect:/viewweapon");
+    }
+
+    @PostMapping(value = "/editweapon")
+    public ModelAndView edit(@RequestParam(value = "weapon_id") String weapon_id) {
+        Weapon weapon = getWeaponById(parseInt(weapon_id));
+        return new ModelAndView("weapon/weaponform", "weapon", weapon);
+    }
+
+
+    @RequestMapping(value = "/deleteweapon", method = RequestMethod.POST)
+    public ModelAndView delete(@RequestParam(value = "weapon_id") String weapon_id) {
+        Weapon weaponToDelete = getWeaponById(parseInt(weapon_id));
+        weaponDao.deleteWeapon(weaponToDelete);
+        weaponList.remove(weaponToDelete);
+        return new ModelAndView("redirect:/viewweapon");
+    }
+
+    @RequestMapping("/viewweapon")
+    public ModelAndView viewweapon(Model model) {
+        return new ModelAndView("viewweapon", "weaponList", weaponList);
+    }
+
+    private Weapon getWeaponById(@RequestParam int weapon_id) {
+        return weaponList.stream().filter(f -> f.getId() == weapon_id).findFirst().get();
+    }
+    
+
 }
